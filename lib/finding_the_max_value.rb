62a05e1bdb7555a8bf966dddd071cfe00004540f
Ariel Grubbs <arielvictor1234@gmail.com>
2020-09-07 00:06:15 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,4 +3,9 @@ def find_max_value(array)
   count = 0
   while count < array.length do
     if array[count] > record_high
+      record_high = array[count]
+    end
+    count += 1
+  end
+  return record_high
 end
